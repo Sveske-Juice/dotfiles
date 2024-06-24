bbe9fd7a03bf0c7d05159ea694be42be53516799
Sveske-Juice <sveske_juice@deprived.dev>
2024-06-24 16:37:35 +0200

    nvim: formatter

---
+++ nvim/.config/nvim/lua/main/lazy/conform.lua
@@ -0,0 +1,15 @@
+return {
+	"stevearc/conform.nvim",
+	config = function()
+		local conform = require("conform")
+		conform.setup({
+			formatters_by_ft = {
+				cpp = { "clang-format" },
+				json = { "jq" },
+				lua = { "stylua" },
+			},
+		})
+
+		vim.keymap.set("n", "<leader>f", conform.format, { desc = "Format buffer" })
+	end,
+}
